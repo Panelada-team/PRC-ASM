3a1d2d02ee0585756f4ebf17a97c47dbb67509f2
marcopereirajunior38@gmail.com <marcopereirajunior38@gmail.com>
2023-08-18 17:14:17 -0300

    WTF

--- source/__init__.py
+++ source/__init__.py
@@ -1,54 +1,73 @@
 instructions_list = {
-        "LOAD":  "00000000",
-        "ADD":   "00000001",
-        "SUB":   "00000010",
-        "JUMP":  "00000100",
-        "REL":   "00001000",
-        "COMP":  "00010000",
-        "IZ":    "00100000",
-        "NZ":    "01000000",
-        "POP":   "10000000",
-        "STORE": "10000001",
-        "VAR":   "10000010",
-        "LOG":   "10000100",
-        "RA1":   "10001000",
-        "RA2":   "10010000",
-        "RB1":   "10100000",
-        "RB2":   "11000000",
-        "RC1":   "11000001",
-        "RC2":   "11000010"
-    }
-    
-    def assemble_instructions(line):
-        opcode = line.split()[0]
-        if opcode in instructions_list:
-            return instructions_list[opcode]
+    "LOAD":  "00000000 ",
+    "ADD":   "00000001 ",
+    "SUB":   "00000010 ",
+    "JUMP":  "00000100 ",
+    "REL":   "00001000 ",
+    "COMP":  "00010000 ",
+    "IZ":    "00100000 ",
+    "NZ":    "01000000 ",
+    "POP":   "10000000 ",
+    "STORE": "10000001 ",
+    "VAR":   "10000010 ",
+    "LOG":   "10000100 ",
+    "RA1":   "10001000 ",
+    "RA2":   "10010000 ",
+    "RB1":   "10100000 ",
+    "RB2":   "11000000 ",
+    "RC1":   "11000001 ",
+    "RC2":   "11000010 ",
+    "ONE":   "11000100 ",
+    "TWO":   "11001000 ",
+    "THREE": "11010000 ",
+    "FOUR":  "11100000 ",
+    "FIVE":  "11100001 ",
+    "SIX":   "11100010 ",
+    "SEVEN": "11100100 ",
+    "EIGHT": "11101000 ",
+    "NINE":  "11110000 ",
+    "ZERO":  "11110001 "
+}
+
+def assemble_instructions(line):
+    words = line.split()
+    binary_instruction = ""
+        
+    for word in words:
+        if word.endswith(","):
+            word = word[:-1]
+        if word in instructions_list:
+            binary_instruction += instructions_list[word]
         else:
-            raise ValueError(f"error on assemble, unknown command: {opcode}")
+            try:
+                binary_instruction += format(int(word), '08b')
+            except ValueError:
+                raise ValueError(f"error on assemble, unknown word: {word}")
+            
+    return binary_instruction
+        
+def assemble_code():
+    code = [
+        "LOAD  RA1, EIGHT",
+        "LOAD  RA2, FOUR",
+        "ADD   RA1, RA2",
+        "STORE RA1",
+        "LOG   RA1"
+    ]
     
-    def assemble_code():
-        code = [
-            "LOAD  RA1, 8",
-            "LOAD  RA2, 4",
-            "ADD   RA1, RA2",
-            "STORE RA1",
-            "LOG   RA1"
-        ]
+    binary_code = []
+            
+    for line in code:
+        bin_instruction = assemble_instructions(line)
+        binary_code.append(bin_instruction)
+                
+    return binary_code
+    
+def main():
+    binary_code = assemble_code()
         
-        binary_code = []
-        
-        for line in code:
-            bin_instructions = assemble_instructions(line)
-            
-            binary_code.append(bin_instructions)
-            
-        return binary_code
-            
-    def main():
-        binary_code = assemble_code()
-        
-        for i, binary_instructions in enumerate(binary_code):
-            print(f"instruction {i + 1}: {binary_instructions}")
-            
-    if __name__ == "__main__":
-        main()
+    for i, binary_instruction in enumerate(binary_code):
+        print(f"instruction {i + 1}: {binary_instruction}")
+    
+if __name__ == "__main__":
+    main()
